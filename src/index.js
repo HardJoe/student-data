cebf720b8782d8d66161e8c0e8a8aeaa16a120e8
HardJoe <jonathan.amadeus@ui.ac.id>
2022-06-13 19:09:37 +0700

    Add database and read endpoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const Student = require('./models/student');
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/read', async (req, res) => {
+  const student = await Student.findOne({ npm: '1111111111' });
+  // const person = new Student({
+  //   name: 'test',
+  //   npm: '1111111111',
+  // });
+
+  // await person.save();
+  res.json(student);
+});
+
+const port = process.env.PORT;
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Student Reader app listening on port ${port}`);
 });
